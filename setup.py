abfc90b17321944a5bc55d3e7d158ae7b9fcbce4
Avraiem Iskandar <a2iskand@uwaterloo.ca>
2025-06-24 22:31:15 -0400

    changed setup.py

--- setup.py
+++ setup.py
@@ -4,5 +4,15 @@ from setuptools import setup, find_packages
 setup(
     name="arctic-atmo-gp",
     version="0.1",
-    packages=find_packages()
+    packages=find_packages(),
+    install_requires=[
+        "numpy",
+        "pandas",
+        "xarray",
+        "netCDF4",
+        "h5netcdf",
+        "matplotlib",
+        "cartopy",    # optional but useful for some HDF5-based files
+        # add other deps here...
+    ],
 )
